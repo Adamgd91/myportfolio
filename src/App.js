fed9fcc675664d1ded7da995eca51c769baf5a0c
Adam Davidson <agdavidson91@gmail.com>
2022-05-22 16:01:11 -0500

    completed About section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import About from "./components/About";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
+
 function App() {
   return (
     <div>
       <NavBar />
       <Home />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-[#205375] text-gray-100">
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+          <div className="sm:text-right pb-8 pl-4">
+            <p className="text-4xl font-bold inline border-b-4 border-[gold] uppercase">
+              About Me
+            </p>
+          </div>
+          <div></div>
+        </div>
+        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+          <div className="sm:text-right text-4xl font-semibold">
+            <p>
+              Hi. I'm Adam Davidson, nice to meet you! Please take a look
+              around!
+            </p>
+          </div>
+
+          <div>
+            <p>
+              I am passionate about building websites and stretching the
+              boundaries of what to do and not to do! I have 2+ years of
+              experince and please feel from to look at my projects to see who I
+              am as a Developer!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
