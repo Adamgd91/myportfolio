74358a1aaf2d7573b232c25f88993da48c8be8cc
Adam Davidson <agdavidson91@gmail.com>
2022-05-22 14:33:56 -0500

    added social icons and styling

---
+++ src/components/Home.jsx
@@ -0,0 +1,8 @@
+import { HiArrowNarrowRight } from "react-icons/hi";
+import React from "react";
+
+const Home = () => {
+  return <div className="w-full h-screen bg-[#205375]">Home</div>;
+};
+
+export default Home;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,19 @@
-import { FaBars, FaTimes } from "react-icons/fa";
+import {
+  FaBars,
+  FaFileCsv,
+  FaGithub,
+  FaLinkedin,
+  FaTimes,
+} from "react-icons/fa";
 
+import { HiOutlineMail } from "react-icons/hi";
 import { useState } from "react";
 
 const NavBar = () => {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#205375] text-gray-300 uppercase">
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#205375] text-gray-100 uppercase">
       <div>
         <header className="font-semibold">
           <span className="text-[gold]">Adam</span> Davidson
@@ -42,8 +49,51 @@ const NavBar = () => {
         <li className="py-6 text-3xl">Work</li>
         <li className="py-6 text-3xl">Contact</li>
       </ul>
-      {/* Social Icons */}
-      <div className="hidden"></div>
+      {/* Social Icons #####if to big on mobile use hidden lg:flex */}
+      <div className="flex fixed flex-col top-[35%] left-0 bg-[#17405c] rounded-r-md">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 ">
+            <a
+              className="flex justify-between items-center w-full"
+              href="https://www.linkedin.com/in/adamgd91/"
+              target="_blank"
+            >
+              Linkedin
+              <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 ">
+            <a
+              className="flex justify-between items-center w-full"
+              href="https://github.com/Adamgd91"
+              target="_blank"
+            >
+              GitHub
+              <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 ">
+            <a
+              className="flex justify-between items-center w-full"
+              href="/https://www.linkedin.com/in/adamgd91/"
+              target="_blank"
+            >
+              Email
+              <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 ">
+            <a
+              className="flex justify-between items-center w-full"
+              href="/https://www.linkedin.com/in/adamgd91/"
+              target="_blank"
+            >
+              Resume
+              <FaFileCsv size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
