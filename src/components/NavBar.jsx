c778a81a6cff36a48ddf111562e3871d708f4c17
Adam Davidson <agdavidson91@gmail.com>
2022-05-22 14:06:10 -0500

    Navbar mobile styling and hamburger added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,10 @@
 import { FaBars, FaTimes } from "react-icons/fa";
 
+import { useState } from "react";
+
 const NavBar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#205375] text-gray-300 uppercase">
       <div>
@@ -10,30 +14,36 @@ const NavBar = () => {
       </div>
 
       {/* NavBar Menu */}
-      <div>
-        <ul className="flex">
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Work</li>
-          <li>Contact</li>
-        </ul>
-      </div>
-
-      {/* Hamburger */}
-      <div className="hidden">
-        <FaBars />
-      </div>
-      {/* Mobile Menu */}
-      <ul className="hidden">
+      {/* Hidden until 768px */}
+      <ul className="hidden md:flex">
         <li>Home</li>
         <li>About</li>
         <li>Skills</li>
         <li>Work</li>
         <li>Contact</li>
       </ul>
+
+      {/* Hamburger */}
+      {/* Anything less than 768px */}
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+      {/* Mobile Menu */}
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#205375] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-3xl">Home</li>
+        <li className="py-6 text-3xl">About</li>
+        <li className="py-6 text-3xl">Skills</li>
+        <li className="py-6 text-3xl">Work</li>
+        <li className="py-6 text-3xl">Contact</li>
+      </ul>
       {/* Social Icons */}
-      <div></div>
+      <div className="hidden"></div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors: {
-        gold: "#F9D923",
-      },
+      //   colors: {
+      //     golds: "#F9D923",
+      //     mainColor: "#205375",
+      //   },
     },
   },
   plugins: [],
